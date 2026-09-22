19955cd7049a0cf10cca9b968da8af915afb002e
thea <kimthea327@gmail.com>
2026-09-22 13:52:53 +0800

    chore: stop tracking NLP files

--- backend/main.py
+++ backend/main.py
@@ -74,7 +74,7 @@ from app.desktop.routers.drafts.fda_verification_drafts import router as fda_ver
 from app.desktop.routers.verification.verification_response import fda_response_router
 
 # Title Extaction Retrieved from the Chrome Extension to NLP
-from app.extension.routers.retrieval import router as retrieval_router
+#from app.extension.routers.retrieval import router as retrieval_router
 
 #for verification history in extension
 from app.extension.routers import verification
@@ -165,7 +165,7 @@ app.include_router(workspace_location_router)
 #         "User": consumer
 #     }
 
-app.include_router(retrieval_router)
+#app.include_router(retrieval_router)
 
 app.include_router(verification.router)
 
