f505519bcee6ccff061ef10dc369f2661b69d91e
Reyyzzz31 <2023304233@pampangastateu.edu.ph>
2026-09-22 13:11:11 +0800

    create backend in geofencing and connect the frontend of geofencing

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,7 @@ from app.desktop.routers.profile_setting import profile as profile_router
 from app.desktop.routers.admin_notifications.admin_notifications import router as admin_notifications_router
 from app.desktop.routers.audit_logs.audit_logs import router as audit_logs_router  
 from app.desktop.routers.notifications.notifications import router as notifications_router  # ADDED     
+from app.desktop.routers.workspace_locations.workspace_location import router as workspace_location_router     
 
 
 from app.database.base import Base
@@ -153,6 +154,7 @@ app.include_router(profile_router.router)
 app.include_router(admin_notifications_router)
 app.include_router(audit_logs_router)
 app.include_router(notifications_router)  # ADDED
+app.include_router(workspace_location_router)
 
 # @app.get("/", status_code=status.HTTP_200_OK)
 # async def user(consumer: consumer_dependency):
